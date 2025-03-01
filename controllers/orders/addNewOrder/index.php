45fd4c6c8cb8e437e68247199bb430f6bfb6d72d
webrstudio <contacto@webrstudio.com>
2025-03-01 13:13:10 -0600

    se actualizan los campos que requiere la tabla usuarios en la bd

--- controllers/orders/addNewOrder/index.php
+++ controllers/orders/addNewOrder/index.php
@@ -20,18 +20,23 @@ try {
         // Leer datos JSON del frontend
         $data = json_decode(file_get_contents("php://input"), true);
 
-        if (!isset($data['usuario_nombre'], $data['usuario_email'], $data['usuario_telefono'], $data['usuario_direccion'], $data['usuario_carrito']) || !is_array($data['usuario_carrito'])) {
+        if (!isset($data['usuario_nombre'], $data['usuario_direccion'], $data['usuario_cp'], $data['usuario_estado'], $data['usuario_municipio'], $data['usuario_colonia'], $data['usuario_email'], $data['usuario_telefono'], $data['usuario_carrito']) || !is_array($data['usuario_carrito'])) {
             http_response_code(400);
             echo json_encode(["error" => "Datos inválidos o incompletos"]);
             exit;
         }
 
         // Insertar usuario en la tabla `usuarios`
-        $stmtUser = $pdo->prepare("INSERT INTO usuarios (usuario_nombre, usuario_email, usuario_telefono, usuario_direccion) VALUES (:usuario_nombre, :usuario_email, :usuario_telefono, :usuario_direccion)");
+        $stmtUser = $pdo->prepare("INSERT INTO usuarios (usuario_nombre, usuario_direccion, usuario_cp, usuario_estado, usuario_municipio, usuario_colonia, usuario_email, usuario_telefono) VALUES (:usuario_nombre, :usuario_direccion, :usuario_cp, :usuario_estado, :usuario_municipio, :usuario_colonia, :usuario_email, :usuario_telefono)");
         $stmtUser->bindParam(":usuario_nombre", $data['usuario_nombre']);
+        $stmtUser->bindParam(":usuario_direccion", $data['usuario_direccion']);
+        $stmtUser->bindParam(":usuario_cp", $data['usuario_cp']);
+        $stmtUser->bindParam(":usuario_estado", $data['usuario_estado']);
+        $stmtUser->bindParam(":usuario_municipio", $data['usuario_municipio']);
+        $stmtUser->bindParam(":usuario_colonia", $data['usuario_colonia']);
         $stmtUser->bindParam(":usuario_email", $data['usuario_email']);
         $stmtUser->bindParam(":usuario_telefono", $data['usuario_telefono']);
-        $stmtUser->bindParam(":usuario_direccion", $data['usuario_direccion']);
+        
         $stmtUser->execute();
         $usuario_id = $pdo->lastInsertId();
 
@@ -40,15 +45,15 @@ try {
         }
 
         // Insertar cada producto del carrito en la tabla `pedidos`
-        $stmtPedido = $pdo->prepare("INSERT INTO pedidos (usuario_id, producto_id, producto_fecha) VALUES (:usuario_id, :producto_id, :producto_fecha)");
+        $stmtPedido = $pdo->prepare("INSERT INTO pedidos (usuario_id, producto_id, pedido_fecha) VALUES (:usuario_id, :producto_id, :pedido_fecha)");
 
         foreach ($data['usuario_carrito'] as $item) {
-            if (!isset($item['producto_id'], $item['producto_fecha'])) {
+            if (!isset($item['producto_id'], $item['pedido_fecha'])) {
                 throw new Exception("Datos de producto incorrectos.");
             }
             $stmtPedido->bindParam(":usuario_id", $usuario_id);
             $stmtPedido->bindParam(":producto_id", $item['producto_id']);
-            $stmtPedido->bindParam(":producto_fecha", $item['producto_fecha']);
+            $stmtPedido->bindParam(":pedido_fecha", $item['pedido_fecha']);
             $stmtPedido->execute();
         }
 
