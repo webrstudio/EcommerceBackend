82108cbfdde22c5e86035b61a5cc43672bc2f0f8
webrstudio <contacto@webrstudio.com>
2025-03-14 18:45:40 -0600

    se agrega la primer vista del dasboard

---
+++ dashboard/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/index.css">
+    <title>Tablero</title>
+</head>
+<body>
+    <main>
+        <section class="container">
+            <h1 class="title">
+                Bienvenido
+            </h1>
+            <div class="optionWrapper flexContainer">
+                <a href='/' class="optionCard boxShadow borderRadius container">
+                    <i class="bi bi-truck optionCardIcon"></i>
+                    <span>Gestionar pedidos</span>
+                </a>
+                <a href='/' class="optionCard boxShadow borderRadius container">
+                    <i class="bi bi-box optionCardIcon"></i>
+                    <span>Gestionar productos</span>
+                </a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
